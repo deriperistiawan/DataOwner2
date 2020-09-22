92ef777e38415d72ccc1f70679078d3471741c3d
deriperistiawan <deritialisperistiawan@gmail.com>
2020-09-22 16:05:53 +0800

    Belajar hari kedua menambah controller
    Controller dibuat untuk mengingat kembali materi minggu lalu

---
+++ app/Http/Controllers/PlnController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PlnController extends Controller
+{
+    public function TambahDaya()
+    {
+        return 'selamat datang di Riau Deri Tialis Peristiawan';
+    }
+}
